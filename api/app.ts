3e821b59496a460346182e387862df70c180af04
Cairo de Paula Vieira <vieira.cairo@hotmail.com>
2021-04-17 00:04:05 -0300

    Adicionando cors

--- api/app.ts
+++ api/app.ts
@@ -2,10 +2,12 @@ import "reflect-metadata";
 import createConnection from "./database";
 import express from "express";
 import { routes } from "./routes";
+import cors from "cors";
 
 createConnection();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
